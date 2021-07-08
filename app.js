d598eb8e139a2c4cc08da9549fcbec23031848dd
Viki Yaputra <minionm1@Vikis-Mac-mini-M1.local>
2021-07-08 14:09:20 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-require('dotenv').config()
+if(process.env.NODE_ENV !== "production"){
+    require('dotenv').config()
+}
+
+
 const express = require(`express`)
 const app = express()
 const router = require(`./routes/routes.js`)
-const port = 3000
+const port = process.env.PORT || 3000
 const cors = require(`cors`)
 const { errorHandler } = require('./middlewares/errorHandler.js')
 
